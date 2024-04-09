4dcee7e1f9a1287ce786f40dcb9ebfb9b73d4f85
mqfazil <fazilq22@gmail.com>
2024-04-09 17:47:12 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("The factorial of 5 = " + fact(5));
         System.out.println("The fibonacci of 5 = " + fib(5));
+        System.out.println("The number of zeros are = " + numberOfZeros(500));
     }
     public static int fact(int n){ // factorial calc
         System.out.println("The current n value = " + n);
@@ -20,5 +21,17 @@ public class Main {
         else{
             return fib(n - 1) + fib(n - 2);
         }
+    }
+    public static int numberOfZeros(int n){
+        int zeroCount;
+        if (n==0)
+            zeroCount = 1;
+        else if (n < 10) // and not 0
+            zeroCount = 0; // 0 for no zeros
+        else if (n%10 == 0)
+            zeroCount = numberOfZeros(n/10) + 1;
+        else // n%10 != 0
+            zeroCount = numberOfZeros(n/10);
+        return zeroCount;
     }
 }
