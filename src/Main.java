3630004827d012e60e5535d7ba9a76b2af4aef33
mqfazil <fazilq22@gmail.com>
2024-04-09 14:38:55 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("The factorial of 5 = " + fact(5));
+        System.out.println("The fibonacci of 5 = " + fib(5));
     }
-    public static int fact(int n){
+    public static int fact(int n){ // factorial calc
         System.out.println("The current n value = " + n);
         if (n==1){
             return 1;
         }
         else{
             return n * fact(n-1);
+        }
+    }
+    public static int fib(int n){ // fibonacci calc
+        System.out.println("The current n value = " + n);
+        if (n== 2 || n == 1) {
+            return 1;
+        }
+        else{
+            return fib(n - 1) + fib(n - 2);
         }
     }
 }
